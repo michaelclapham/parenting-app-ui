861ef635c1f5181ee5be2daa18038dc2c5806523
Michael Clapham <michaeljclapham@gmail.com>
2020-07-15 17:13:15 +0100

    Hide some hats

--- src/app/shared/components/blobs/blob2/blob2.component.ts
+++ src/app/shared/components/blobs/blob2/blob2.component.ts
@@ -4,19 +4,31 @@ import { AnimationOptions } from 'ngx-lottie';
 @Component({
   selector: 'plh-blob2',
   templateUrl: './blob2.component.html',
-  styleUrls: ['./blob2.component.scss'],
+  styleUrls: ['./blob2.component.scss']
 })
 export class Blob2Component implements OnInit {
 
   animOptions: AnimationOptions = {
-    path: "assets/lottie-anims/test_rig_rectangle_empty_box.json",
-    autoplay: true
+    path: "assets/lottie-anims/test_body_motion_only.json",
+    autoplay: true,
+    loop: 30
   };
 
   // setInterval(() => { console.log("lolz ", $('g.empty_layer > g').transform.animVal[0].matrix); })
-  
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+
+  }
+
+  animationCreated() {
+    setTimeout(() => {
+      for (var cls of ["red_hat", "green_hat", "blue_hat"]) {
+        var svgGroup = document.getElementsByClassName(cls)[0] as SVGGeometryElement;
+        svgGroup.style.display = "none";
+      }
+    }, 2000);
+  }
 
 }
